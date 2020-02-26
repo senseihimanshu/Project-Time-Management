9d01a149fcdd53f754996a2b1baa7ae38172a9ad
balani98 <balanideepanshu92@gmail.com>
2020-02-26 19:24:02 +0530

    schemas added

---
+++ server/schemas/employee.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const project=require('./project-details');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+module.exports={
+    
+    name:{
+        type:String,
+        default:null
+  },
+    designation:{
+         type:String,
+         default:'consultant 1',
+         enum:['consultant 1 ','consultant 2','associate 1','associate 2']
+    },
+    joining_date:{
+      type:Date,
+      default:null
+    },
+    contactNo:{
+      type:String,
+      default:null
+    },
+    address:{
+      type:String,
+      default:null
+    },
+    email:{
+      type:String,
+      required:true 
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    projectId:[{
+        type:ObjectId,
+        ref:"project",
+        default:null
+    }]
+
+    
+}
