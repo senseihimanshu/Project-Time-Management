8162dab32b19ccb06c09bbade9f8177dedb89018
Deepakyadav19 <dy30152@gmail.com>
2020-02-27 00:00:07 +0530

    add,update,delete,listing api,dashboard page done..API tested

---
+++ server/schemas/employee.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: {
+      type: String,
+      default: null
+    },
+    phoneNo: {
+      type: String
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    designation: {
+      type: String,
+      default: "consultant 1"
+    },
+    password: {
+      type: String,
+      default: null
+    }
+  };
+  
