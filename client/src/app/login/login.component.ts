f33ec71884d488b6dbe9e4882f65489e185b3a1d
abharana24 <abha801@gmail.com>
2020-02-27 21:15:54 +0530

    resolved minor errors

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -49,16 +49,16 @@ export class LoginComponent implements AfterViewInit{
           else{
             const role=decodeToken.data.role[0];
             console.log(role);
-            if(role=='employee'){
+            if(role=='Employee'){
                 this.router.navigate(['/employee']);
             }
-            else if (role=='admin'){
+            else if (role=='Admin'){
               this.router.navigate(['/admin']);
             }
-            else if( role=='C-level manager'){
+            else if( role=='C-level Manager'){
               this.router.navigate(['/clevel']);
             }
-            else if(role=='Project manager'){
+            else if(role=='Project Manager'){
               this.router.navigate(['/manager']);
             }
             else 
