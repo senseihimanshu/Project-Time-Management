c82a3967c88d0a208eaa8eaefa50c1b6dc9ce19f
Kritika Sachdeva <kritika.sachdeva@cybergindia.local>
2020-02-27 12:19:12 +0530

    role based authorisation using route guards

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { AuthorizationService } from './services/authorization.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 //Third Party Components
 import { MDBBootstrapModule, CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule } from 'angular-bootstrap-md';
@@ -10,23 +13,39 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './main/login/login.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
+import { ReviewComponent } from './review/review.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    TimesheetComponent,
+    ReviewComponent,
+    DashboardComponent,
+    AdminComponent,
+    AccessDeniedComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     FormsModule, 
+    HttpClientModule,
+    JwtModule.forRoot({}),
     //Third Party
     MDBBootstrapModule.forRoot(),
-    CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule,
+    CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule, AppRoutingModule,
   ],
-  providers: [],
+  providers: [AuthorizationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/timesheet/timesheet.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timesheet',
+  templateUrl: './timesheet.component.html',
+  styleUrls: ['./timesheet.component.scss']
+})
+export class TimesheetComponent implements OnInit {
+
+  constructor() { }
+  loading = false;
+  //  users: User[] = [];
+
+  //  constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.loading = true;
+        // this.userService.getAll().pipe(first()).subscribe(users => {
+        //     this.loading = false;
+        //     this.users = users;
+        // });
+    }
+}
