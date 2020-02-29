df41349e16418214e1d22daf3fe5ce524ce241ab
senseihimanshu <hsharma151197@gmail.com>
2020-02-29 21:23:58 +0530

    Added Complete timesheet form with input validations on the go

--- client/src/app/timesheet/timesheet.module.ts
+++ client/src/app/timesheet/timesheet.module.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
     ],
     imports: [
         FormsModule,
-        CommonModule
+        CommonModule,
     ],
     exports: [
         TimesheetComponent
