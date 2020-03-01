99ba2c868d3295108905488631ddf595490f0e59
abharana24 <abha801@gmail.com>
2020-03-01 21:00:08 +0530

    completed user profile UI,services and api

--- client/src/app/myprofile/myprofile.component.ts
+++ client/src/app/myprofile/myprofile.component.ts
@@ -17,13 +17,26 @@ export class MyprofileComponent implements OnInit {
     private route: ActivatedRoute) {  }
     
   ngOnInit(): any {
+    const token = localStorage.getItem('Authorization');
+      
+    // //Decode JWT and return the Payload in JSON Format
+   const decodeToken= this.jsonDecoder(token);
+   console.log(decodeToken);
+   console.log(decodeToken.data.empId);
+   
+   return this._service.getEmployee(decodeToken.data.empId).subscribe((response: any) => {
+    console.log(response);
+        console.log(response.payload.employee);
+     return (this.employee = response.payload.employee);
+      });
+}
 
-   // return this._service.getEmployee(params.empId);
-  }//)
-//)
-// .subscribe((response: any) => {
-//   console.log(response);
-//   console.log(response.payload.employee);
-//   return (this.employee = response.payload.employee);
-// });
+jsonDecoder = (token) => {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+};
 }
