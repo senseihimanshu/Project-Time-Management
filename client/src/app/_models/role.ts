da171d9bae02a09b1ac20d52fa0612b904a9a9cc
abharana24 <abha801@gmail.com>
2020-03-05 12:53:37 +0530

    timesheet component changed

--- client/src/app/_models/role.ts
+++
@@ -1,6 +0,0 @@
-export enum Role {
-    Employee = 'Employee',
-    Admin = 'Admin',
-    Clevel= 'C Level Manager',
-    ProjectManager= 'Project Manager'
-}
--- client/src/app/timesheet/timesheet.component.ts
+++ client/src/app/timesheet/timesheet.component.ts
@@ -1,5 +1,5 @@
 import { MyprofileComponent } from './../myprofile/myprofile.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef,ViewChild } from '@angular/core';
 
 
 @Component({
@@ -11,9 +11,13 @@ export class TimesheetComponent implements OnInit {
   timesheet: any;
 
   constructor(){}
-
+ // @ViewChild('dt-less-columns', {static: false}) 'dt-less-columns': ElementRef;
   ngOnInit(): void{}
-
+  // '#dt-less-columns'.mdbEditor({
+  //   modalEditor: true,
+  //   headerLength: 5,
+  //   });
+  //   $('.dataTables_length').addClass('bs-select');
   submitToApi(event){
     console.log(event);
   }
