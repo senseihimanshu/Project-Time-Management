a2c31ac10797eda5d1bcec4b37778bd8ea6e0c7b
Deepakyadav19 <dy30152@gmail.com>
2020-03-11 19:33:51 +0530

    timesheet component designed

--- client/src/app/timesheet/timesheet.component.ts
+++ client/src/app/timesheet/timesheet.component.ts
@@ -14,7 +14,7 @@ import * as moment from "moment";
 import { MatDialog } from "@angular/material/dialog";
 import { EmployeeService } from "../services/employee.service";
 import { SendHttpRequestService } from "../services/send-http-request.service";
-
+import { Router, RouterLink } from "@angular/router";
 @Component({
   selector: "table-editable",
   templateUrl: "./timesheet.component.html",
@@ -32,7 +32,10 @@ export class TimesheetComponent implements OnInit {
   closeResult: string;
 
   empObjId: string;
-
+  name = "Angular";
+  page = 1;
+  pageSize = 10;
+  items = [];
 
   menus: any = [
     {
@@ -228,7 +231,8 @@ export class TimesheetModal implements OnInit {
   constructor(
     private employeeService: EmployeeService,
     private timesheetService: TimesheetService,
-    private httpService: SendHttpRequestService
+    private httpService: SendHttpRequestService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -318,5 +322,64 @@ export class TimesheetModal implements OnInit {
   handleProjectData(project: any) {
     console.log(project);
     this.project = project;
+  }
+  logout() {
+    this.router.navigate(["/login"]);
+  }
+  myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    console.log(input, "hgcfdfcj");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /* Make a loop that will continue until
+  no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+    first, which contains table headers): */
+      for (i = 1; i < rows.length - 1; i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[1];
+        y = rows[i + 1].getElementsByTagName("TD")[1];
+        // Check if the two rows should switch place:
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
   }
 }
